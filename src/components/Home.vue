27d3cd0615f96c8e827e7f4ec591c19b8a1baa87
Jerome Joseph <jerome.joseph@techverito.com>
2020-03-28 21:02:07 +0530

    Removing codepen icons from home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -38,42 +38,7 @@
         class="fill-height"
         fluid
       >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/zgxeLQ"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
+        <PrayerCard/>
       </v-container>
     </v-content>
     <v-footer
@@ -86,8 +51,12 @@
 </template>
 
 <script>
+  import PrayerCard from "./PrayerCard"
   export default {
     name: "Home",
+    components: {
+      PrayerCard
+    },
     props: {
       source: String,
     },
---
+++ src/components/PrayerCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <PrayerContent/>
+  </div>
+</template>
+
+<script>
+  import PrayerContent from "./PrayerContent"
+  export default {
+    name: "PrayerCard",
+    components: {
+      PrayerContent
+    }
+  }
+</script>
